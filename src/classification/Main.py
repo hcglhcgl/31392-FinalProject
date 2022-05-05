8d5f77b22d186073483dfcb5d5830f529eb8c4d8
Sana Mahmoud <sana1997cr7@gmail.com>
2022-05-05 22:21:08 +0200

    Function main changed to have a function to detect and classify. And classes.txt corrected.

--- src/classification/Main.py
+++ src/classification/Main.py
@@ -2,45 +2,6 @@ import cv2
 import argparse
 import numpy as np
 
-# handle command line arguments
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True,
-                help = './right/1585434310_271912098_Right.png')
-ap.add_argument('-c', '--config', required=True,
-                help = './yolov4-obj.cfg')
-ap.add_argument('-w', '--weights', required=True,
-                help = 'backup/yolov4-obj_last.weights')
-ap.add_argument('-cl', '--classes', required=True,
-                help = './obj.names')
-args = ap.parse_args()
-
-
-image = cv2.imread(args.image)
-
-Width = image.shape[1]
-Height = image.shape[0]
-scale = 0.00392
-
-# read class names from text file
-classes = None
-with open(args.classes, 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
-
-# generate different colors for different classes 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-# read pre-trained model and config file
-net = cv2.dnn.readNet(args.weights, args.config)
-
-# create input blob 
-blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
-
-# set input blob for the network
-net.setInput(blob)
-
-
-
-
 # function to get the output layer names 
 # in the architecture
 def get_output_layers(net):
@@ -52,7 +13,9 @@ def get_output_layers(net):
     return output_layers
 
 # function to draw bounding box on the detected object with class name
-def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+def draw_bounding_box(img, class_id, classes, confidence, x, y, x_plus_w, y_plus_h):
+        # generate different colors for different classes 
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
     label = str(classes[class_id])
 
@@ -62,66 +25,89 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
     
+def detect_object(pic, cfg, weight_file, class_file):
+    print(pic)
+    image = cv2.imread(pic)
 
-# run inference through the network
-# and gather predictions from output layers
-outs = net.forward(get_output_layers(net))
-# print(outs)
-# initialization
-class_ids = []
-confidences = []
-boxes = []
-conf_threshold = 0.01
-nms_threshold = 0.4
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
 
-# for each detetion from each output layer 
-# get the confidence, class id, bounding box params
-# and ignore weak detections (confidence < 0.5)
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        # print(detection[4:])
-        class_id = np.argmax(scores)
-        # print(scores)
-        confidence = scores[class_id]
-        # print(confidence)
-        if confidence > 0.01:
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            class_ids.append(class_id)
-            confidences.append(float(confidence))
-            boxes.append([x, y, w, h])
-            
-            
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-# print(indices)
-print(confidences)
-print(class_ids)
-# go through the detections remaining
-# after nms and draw bounding box
-for i in indices:
-    # print(i)
-    # i = i[0]
-    box = boxes[i]
-    x = box[0]
-    y = box[1]
-    w = box[2]
-    h = box[3]
-    
-    draw_bounding_box(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
-print("hello")
-# display output image    
-cv2.imshow("object detection", image)
+    # read class names from text file
+    classes = None
+    with open(class_file, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
 
-# wait until any key is pressed
-cv2.waitKey()
-    
- # save output image to disk
-# cv2.imwrite("object-detection.jpg", image)
+    # read pre-trained model and config file
+    net = cv2.dnn.readNet(weight_file, cfg)
 
-# release resources
-# cv2.destroyAllWindows()
+    # create input blob 
+    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+    # set input blob for the network
+    net.setInput(blob)
+
+    # run inference through the network
+    # and gather predictions from output layers
+    outs = net.forward(get_output_layers(net))
+    # print(outs)
+    # initialization
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    # for each detetion from each output layer 
+    # get the confidence, class id, bounding box params
+    # and ignore weak detections (confidence < 0.5)
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            # print(detection[4:])
+            class_id = np.argmax(scores)
+            # print(scores)
+            confidence = scores[class_id]
+            # print(confidence)
+            if confidence > 0.01:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+                
+                
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    # print(indices)
+    # print(confidences)
+    # print(class_ids)
+    # go through the detections remaining
+    # after nms and draw bounding box
+    for i in indices:
+        # print(i)
+        # i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        
+        draw_bounding_box(image, class_ids[i], classes, confidences[i], round(x), round(y), round(x+w), round(y+h))
+    # display output image    
+    cv2.imshow("object detection", image)
+
+    # wait until any key is pressed
+    cv2.waitKey()
+        
+    # save output image to disk
+    # cv2.imwrite("object-detection.jpg", image)
+
+    # release resources
+    # cv2.destroyAllWindows()
+    return max(set(class_ids), key = class_ids.count)
+
+print(detect_object('/home/qaituli/Desktop/31392-FinalProject/Stereo_conveyor_without_occlusions/right/1585434298_556632042_Right.png', '/home/qaituli/Desktop/31392-FinalProject/src/classification/yolov4-obj.cfg', '/home/qaituli/Desktop/31392-FinalProject/src/classification/backup/yolov4-obj_last.weights', '/home/qaituli/Desktop/31392-FinalProject/src/classification/classes.txt'))
