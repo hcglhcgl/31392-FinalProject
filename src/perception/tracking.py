b4c65475e3e56a8d8717bc997152e410eb310ca2
patrickvibild <patrickvibild@gmail.com>
2022-05-08 20:44:22 +0200

    add new caching to tracking so we predict the bounding box when the object starts to be obscure.

--- src/perception/tracking.py
+++ src/perception/tracking.py
@@ -3,9 +3,11 @@ import math
 import cv2
 import numpy as np
 
+
 def background_preprocess(frame, ksize=5):
     frame = cv2.GaussianBlur(frame, (ksize, ksize), 2)
     return frame
+
 
 def contour(static_img, dilate):
     kernel = np.ones((3, 3), np.uint8)
@@ -35,7 +37,6 @@ def moving_features(prev_frame, frame):
     def distance_points(x1, y1, x2, y2):
         return math.hypot(x2 - x1, y2 - y1)
 
-
     features = []
     feat1 = cv2.goodFeaturesToTrack(prev_frame, maxCorners=700, qualityLevel=0.0015, minDistance=6)
     if feat1 is None:
@@ -50,6 +51,7 @@ def moving_features(prev_frame, frame):
             features.append(feat2[i])
     return features
 
+
 def display_features(movement, frame, features):
     for feature in features:
         cv2.circle(frame, (int(feature[0][0]), int(feature[0][1])), 5, (0, 255, 0), -1)
@@ -58,21 +60,83 @@ def display_features(movement, frame, features):
     cv2.imshow('show', display)
     cv2.waitKey(500)
 
-def display_boxes(movement, frame, boxes):
-    for box in boxes:
-        x, y, w, h = cv2.boundingRect(box)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
+def display_boxes(movement, frame, x, y, w, h):
+    if x == 0 and y == 0:
+        pass
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
     display = np.hstack((frame, cv2.cvtColor(movement, cv2.COLOR_GRAY2BGR)))
     cv2.imshow('show', display)
     cv2.waitKey(500)
     return frame
+
 
 def find_objects(tracked_movement):
     cnts = cv2.findContours(tracked_movement, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     return cnts
 
+
+def filter_contours(cnts):
+    filtered = []
+    for c in cnts:
+        x, y, w, h = cv2.boundingRect(c)
+        if w > 50 and h > 50:
+            filtered.append(c)
+    return filtered
+
+
+class Tracking:
+    def __init__(self):
+        self.x = self.y = self.h = self.w = 0
+
+    def track(self, cnts):
+        # first we obtain biggest contour
+        if cnts is None or len(cnts) == 0:
+            return 0, 0, 0, 0
+        big_dim = 0
+        for c in cnts:
+            x, y, w, h = cv2.boundingRect(c)
+            if w + h >= big_dim:
+                big = c
+                big_dim = w + h
+
+        (x, y, w, h) = self.__generate_box(big)
+        if self.__is_close(big):
+            self.__cache(x, y, w, h)
+        else:
+            cx, cy, cw, ch = cv2.boundingRect(big)
+            self.__cache(cx, cy, cw, ch)
+        return x, y, w, h
+
+    def __generate_box(self, cnts):
+        x, y, w, h = cv2.boundingRect(cnts)
+        # if the original box is already X pixels in w then its to shrinking to much to predict
+        if w < 20:
+            return x, y, w, h
+        wshift = max(self.w - w, 0)
+        return x - wshift, y, w + wshift, h
+
+    def __cache(self, x, y, w, h):
+        """
+        :param cnts: contour that is been caches in tracking.
+        :return: None
+        """
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def __is_close(self, cnts):
+        """
+        :param cnts: passing contour
+        :return: returning boolean if previous contour is close to this one. We take into consideration width shrinking
+        """
+        x, y, w, h = cv2.boundingRect(cnts)
+        wshift = max(self.w - w, 0)
+        # if x of the contour has moved less than 30 pixels (counting the contour shrink)
+        # checking also Y coordinate.
+        return (self.x + wshift - x) < 30 and self.y - y < 50
 
 
 def main(start, record):
@@ -91,17 +155,16 @@ def main(start, record):
         cv2.imshow('static', static_background)
         cv2.waitKey(500)
 
-
-
+    tracker = Tracking()
     # processing now the video.
-    left_imgs = left_imgs[70+start: -1]
+    left_imgs = left_imgs[70 + start: -1]
     for idx, img in enumerate(left_imgs):
         # skipping first tracking
         if idx == 0:
             frame = cv2.imread(img)
             if record:
                 height, width, layers = frame.shape
-                out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, (width, height))
+                out = cv2.VideoWriter('project_1.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, (width, height))
             previous_img = cv2.imread(img)
             mask = contour(previous_img, 1)
             previous_movement = track_movement_masks(static_background, mask)
@@ -112,12 +175,17 @@ def main(start, record):
         mask = crop_left(mask)
         tracked_movement = track_movement_masks(static_background, mask)
         boxes = find_objects(tracked_movement)
-        display = display_boxes(tracked_movement, frame, boxes)
-        out.write(frame)
+        boxes = filter_contours(boxes)
+        (x, y, w, h) = tracker.track(boxes)
+        display = display_boxes(tracked_movement, frame, x, y, w, h)
+        if record:
+            out.write(frame)
 
         previous_movement = tracked_movement
-    out.release()
+    if record:
+        out.release()
+
 
 if __name__ == '__main__':
     # you can pass here the frame number and it will start from that frame. Useful for debugging ...
-    main(0, False)
+    main(0, True)
